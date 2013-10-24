7001421b5e9f641e3a313937c52f2a21eaa30771
Ricardo Silva <ricardo@salamandra.cc>
2013-10-24 08:01:04 -0200

    Changed api url path

--- fiscalcidadao/urls.py
+++ fiscalcidadao/urls.py
@@ -36,11 +36,11 @@ admin.autodiscover()
 
 # Routers provide an easy way of automatically determining the URL conf
 router = routers.DefaultRouter()
-router.register(r'api/facts', FactViewSet)
-router.register(r'api/occurrences', OccurrenceViewSet)
+router.register(r'facts', FactViewSet)
+router.register(r'occurrences', OccurrenceViewSet)
 
 urlpatterns = patterns('',
-    url(r'^', include(router.urls)),
+    url(r'^api/', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^api-auth/', include('rest_framework.urls',
