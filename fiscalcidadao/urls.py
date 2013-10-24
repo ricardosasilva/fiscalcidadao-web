c0a5dcd0be049bbffec1a6ee75a7d47078271166
Ricardo Silva <ricardo@salamandra.cc>
2013-10-24 07:45:48 -0200

    Fixed facts api url

--- fiscalcidadao/urls.py
+++ fiscalcidadao/urls.py
@@ -37,7 +37,7 @@ admin.autodiscover()
 # Routers provide an easy way of automatically determining the URL conf
 router = routers.DefaultRouter()
 router.register(r'api/facts', FactViewSet)
-router.register(r'occurrences', OccurrenceViewSet)
+router.register(r'api/occurrences', OccurrenceViewSet)
 
 urlpatterns = patterns('',
     url(r'^', include(router.urls)),
