bdb0ee0e800bd2458fa0847e33eaa259351aca83
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2013-10-27 13:32:06 -0200

    Fixing configurations

--- fiscalcidadao/settings/gunicorn.py
+++ fiscalcidadao/settings/gunicorn.py
@@ -2,6 +2,6 @@ import multiprocessing
 import sys
 from os.path import dirname
 
-django_settings = 'dev.diogo'
+django_settings = 'fiscalcidadao'
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
