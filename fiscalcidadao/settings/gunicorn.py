f8e54e5897d088c07c401db3c520d4bc4986223e
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2013-10-27 13:55:26 -0200

    Removing useless config from gunicorn

--- fiscalcidadao/settings/gunicorn.py
+++ fiscalcidadao/settings/gunicorn.py
@@ -3,6 +3,5 @@ import sys
 from os.path import dirname
 
 sys.path.append(dirname(dirname(dirname(__file__))))
-django_settings = 'fiscalcidadao'
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
